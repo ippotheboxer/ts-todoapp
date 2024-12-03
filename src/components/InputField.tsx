92604e69c70067d2b4d60d7b47ee92cf9991c089
Sophie <sophiesokol123@gmail.com>
2024-12-03 23:35:43 +0000

    Handle input submit

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const InputField = () => {
-    const buttonClick = (e: any) => {
-        e.preventDefault()
-    }
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+
   return (
-    <form className='flex w-11/12 relative items-center'>
-        <input type="text" placeholder='Enter a task' className='inputBox'/>
-        <button className='inputSubmit' onClick={(e) => buttonClick(e)}>+</button>
+    <form className='flex w-11/12 relative items-center' onSubmit={handleAdd}>
+        <input type="text" 
+        placeholder='Enter a task' 
+        value={todo} 
+        onChange={(e) => setTodo(e.target.value)}
+        className='inputBox'/>
+        <button 
+        className='inputSubmit' 
+        type='submit'>+</button>
     </form>
   )
 }
