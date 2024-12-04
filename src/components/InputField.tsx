b915fe3b036ab03687dca62221044346cd1688b9
Sophie <sophiesokol123@gmail.com>
2024-12-04 20:53:17 +0000

    Style component when adding

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface Props {
     todo: string;
@@ -7,10 +7,16 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='flex w-11/12 relative items-center' onSubmit={handleAdd}>
-        <input type="text" 
+    <form 
+    className='flex w-11/12 relative items-center' 
+    onSubmit={(e) => { handleAdd(e); 
+    inputRef.current?.blur()}}>
+        <input 
+        ref={inputRef}
+        type="text" 
         placeholder='Enter a task' 
         value={todo} 
         onChange={(e) => setTodo(e.target.value)}
